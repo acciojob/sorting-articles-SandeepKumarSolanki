f91f0f3600ec34006bd7da31a237074ca4b19750
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-25 10:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+let ul = document.querySelector('ul');
+
+bands.sort((a,b) => a-b);
+for(let t of bands){
+	let li = document.createElement('li');
+	li.innerText = t;
+	ul.append(li)
+}
+
+
+
