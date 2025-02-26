463786f6bdcd9a9d295c4d6579a4f5cad4732775
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-26 14:20:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+ let ul = document.getElementById('band');
 
-let ul = document.getElementById('band');
+        const bands = [
+            'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean',
+            'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans',
+            'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+        ];
 
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+        // Function to remove 'a', 'an', and 'the' from the beginning of band names
+        function removeArticle(band) {
+            return band.replace(/^(a|an|the)\s+/i, '');
+        }
 
-let sortedString = bands.sort((a,b)=> a-b);
+        // Sort the bands, ignoring 'a', 'an', and 'the'
+        let sortedBands = bands.sort((a, b) => {
+            let bandA = removeArticle(a).toLowerCase();
+            let bandB = removeArticle(b).toLowerCase();
+            if (bandA < bandB) return -1;
+            if (bandA > bandB) return 1;
+            return 0;
+        });
 
-sortedString.forEach((items) => {
-	let li = document.createElement('li');
-	li.innerText = `${items}`;
-	ul.append(li);
-})
-
-
+        // Create list items for each band and append them to the unordered list
+        sortedBands.forEach((band) => {
+            let li = document.createElement('li');
+            li.innerText = band;
+            ul.appendChild(li);
+        });
